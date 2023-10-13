545b70b95b2c8c0084d4019ff46f835f5592374a
Alessandra Belló <alessandrabellosoares@gmail.com>
2023-10-13 11:56:28 -0300

    Doctest adicionado

--- analise_alessandra/funcoes_selecao.py
+++ analise_alessandra/funcoes_selecao.py
@@ -1,3 +1,5 @@
+import doctest as dt
+import pandas as pd
 def selecionar_colunas(dataframe, *nomes_colunas):
     """
     A função selecionar colunas é utilizada para selecionar colunas específicas de um Dataframe.
@@ -13,6 +15,14 @@ def selecionar_colunas(dataframe, *nomes_colunas):
     -------
     df : DataFrame
         DataFrame sem as colunas indicadas.
+        
+    Exemplo
+    -------
+    >>> df = pd.DataFrame({"nome":["Alessandra", "Guilherme"], "Idade": [20,21]})
+    >>> selecionar_colunas(df, "nome")
+             nome
+    0  Alessandra
+    1   Guilherme
 
     """
     df = dataframe[[*nomes_colunas]]
@@ -36,6 +46,14 @@ def trocar_valor (dataframe, coluna, valores):
     dataframe : Dataframe
         Dataframe com os valores alterados.
 
+    Exemplo
+    -------
+    >>> df = pd.DataFrame({"nome":["Alessandra", "Guilherme"], "Idade": [20,21]})
+    >>> trocar_valor(df, "Idade", {20:"Vinte", 21: "Vinte e um"})
+             nome       Idade
+    0  Alessandra       Vinte
+    1   Guilherme  Vinte e um
+
     """
     dataframe[coluna] = dataframe[coluna].map(valores)
     return dataframe
@@ -57,7 +75,20 @@ def somar_valores_agrupado (dataframe, coluna, coluna_agrupamento):
     -------
     df : Series
         Serie com a soma da coluna, onde o índice é o agrupamento.
+        
+    Exemplo
+    -------
+    >>> df = pd.DataFrame({"Idade":[20,21,20,21], "Itens comprados":[3,5,7,2]})
+    >>> somar_valores_agrupado (df, "Itens comprados", "Idade")
+    Idade
+    20    10
+    21     7
+    Name: Itens comprados, dtype: int64
 
     """
     df = dataframe.groupby(coluna_agrupamento)[coluna].sum()
     return df
+
+if __name__ == "__main__":
+    dt.testmod()
+    dt.testmod(verbose=True)
