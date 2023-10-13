0aac9131e00009ce115cf9067876188f298b8ff8
Alessandra Belló <alessandrabellosoares@gmail.com>
2023-10-13 16:56:23 -0300

    Atualizaçao sphinx

--- analise_alessandra/docs/source/conf.py
+++ analise_alessandra/docs/source/conf.py
@@ -14,15 +14,15 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinx.ext.autodoc']
 
 templates_path = ['_templates']
-exclude_patterns = []
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 language = 'pt_BR'
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
