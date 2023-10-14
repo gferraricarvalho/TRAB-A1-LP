d165e29a501a7d13008070060ac8db1a2c18f456
Jeann-Rocha <jeannrochasilva@gmail.com>
2023-10-14 17:59:00 -0300

    Terminando a main.py

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 from limpeza import funcoes as fl
 import ler_arquivo as la
+import os
 
+# lendo a base de dados
 df = la.ler_arquivo_csv("microdados_ed_basica_2021.csv")
 
-# colocar aqui funcoes de limpeza sobre o dataframe criado
+# limpando a base de dados
+df = fl.colunas_indesejadas(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'colunas_nao_utilizadas.txt'), df)
+df = fl.remove_duplicadas(df)
+df = fl.remove_NaN(df)
+df = fl.excluir_colunas_de_zeros(df)
+for col in df.columns:
+    if all(df[col].apply(lambda x: isinstance(x, int))):
+        df = fl.excluir_outliers(df, col)
+
+# salvando a base limpa
+df.to_csv('dados_limpos.csv')
