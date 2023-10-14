d61cbd99f38e0b7814e0f9f9881b805ff0adf22b
Jeann-Rocha <jeannrochasilva@gmail.com>
2023-10-14 03:56:39 -0300

    Ajeitando detalhes do módulo vizualização.py

--- analise_jeann/vizualizacao.py
+++ analise_jeann/vizualizacao.py
@@ -13,19 +13,22 @@ def viz_simples(df: pd.DataFrame, escolha_coluna: str, colunas: list, dado_espec
     if dado_especifico not in [""] + colunas:
         raise ValueError(f'O dado_específico deve ser vazio (nenhum valor atribuído ou string vazia) ou deve ser alguma das colunas: {colunas}')
     
-    df_tend = fu.agrupa_por(df, escolha_coluna)
+    df_tend = fu.agrupa_por(df, escolha_coluna, colunas)
 
     if dado_especifico == "":
-        fig, ax = plt.subplots(4, 6, figsize=(40, 20))
+        fig, ax = plt.subplots(4, 6, figsize=(20, 10))
+        fig.subplots_adjust(hspace=0.5)
         
         cont = 0
         for i in range(0, 4):
             for j in range(0, 6):
                 media = df_tend[colunas[cont]].mean()
                 ax[i, j].bar(df_tend.index, df_tend[colunas[cont]])
-                ax[i, j].set_title(f"{colunas[cont]}")
+                ax[i, j].set_title(f"{colunas[cont]}", fontsize=7)
                 if escolha_coluna == 'SG_UF':
                     ax[i, j].tick_params(axis='x', labelrotation=90)
+                ax[i, j].tick_params(axis='x', labelsize=4)
+                ax[i, j].tick_params(axis='y', labelsize=5)
                 ax[i, j].axhline(media, color='red', linestyle='-', label='Média')
                 cont += 1
     
@@ -52,18 +55,21 @@ def viz_dist_freq(df: pd.DataFrame, escolha_coluna: str, colunas: list, dado_esp
     if dado_especifico not in [""] + colunas:
         raise ValueError(f'O dado_específico deve ser vazio (nenhum valor atribuído ou string vazia) ou deve ser alguma das colunas: {colunas}')
         
-    df_freq = fu.agrupa_por(df, escolha_coluna)
+    df_freq = fu.agrupa_por(df, escolha_coluna, colunas)
     
     if dado_especifico == "":
         fig, ax = plt.subplots(4, 6, figsize=(40, 20))
+        fig.subplots_adjust(hspace=0.5, wspace=0.5)
         
         cont = 0
         for i in range(0, 4):
             for j in range(0, 6):
                 ax[i, j].hist(df_freq[colunas[cont]], bins=10, edgecolor='k')
-                ax[i, j].set_title(f"{colunas[cont]}")
-                ax[i, j].set_xlabel("Quantidade")
-                ax[i, j].set_ylabel("Frequência")
+                ax[i, j].set_title(f"{colunas[cont]}", fontsize=7)
+                ax[i, j].set_xlabel("Quantidade", fontsize=7)
+                ax[i, j].set_ylabel("Frequência", fontsize=7)
+                ax[i, j].tick_params(axis='x', labelsize=4)
+                ax[i, j].tick_params(axis='y', labelsize=4)
                 cont += 1
     
         plt.show()
@@ -77,13 +83,11 @@ def viz_dist_freq(df: pd.DataFrame, escolha_coluna: str, colunas: list, dado_esp
         plt.show()
 
 
-def viz_correlativa(df: pd.DataFrame, escolha_coluna: str, colunas: list):
+def viz_correlativa(df: pd.DataFrame, escolha_coluna: str, colunas_agua: list, colunas_energia: list, colunas_esgoto: list, colunas_lixo: list):
     """Vizualização da Correlação do Tipo de Localização, Tipo de Localização Diferenciada ou Tipo de Dependência com a
     quantidade de cada indicador ecológico utilizado (Água, Energia, Esgoto ou Tratamento de Lixo) em um (ou vários) gráfico(s)
-    de ???
+    de barras verticais empilhadas.
     """
-
-    # tratar a escolha_colunas e colunas...
     
     if escolha_coluna not in ['TP_LOCALIZACAO', 'TP_LOCALIZACAO_DIFERENCIADA', 'TP_DEPENDENCIA']:
         raise ValueError('A escolha_coluna deve ser "TP_LOCALIZACAO" (Tipo de Localização da Escola), "TP_LOCALIZACAO_DIFERENCIADA" (Tipo de Localização Diferenciada da Escola) ou "TP_DEPENDÊNCIA" (Tipo de Dependência da Escola)')
@@ -97,25 +101,25 @@ def viz_correlativa(df: pd.DataFrame, escolha_coluna: str, colunas: list):
     
     fig, ax = plt.subplots(2, 2, figsize=(10, 8))
     
-    df_corr_agua = df[[escolha_coluna] + colunas[0]].groupby(escolha_coluna).agg(lambda x: sum(x))
+    df_corr_agua = df[[escolha_coluna] + colunas_agua].groupby(escolha_coluna).agg(lambda x: sum(x))
     df_corr_agua = df_corr_agua.rename(index = idx)
     df_corr_agua.plot(kind='bar', stacked=True, figsize=(12, 10), ax=ax[0,0])
     ax[0, 0].tick_params(axis='x', labelrotation=0)
     ax[0, 0].legend(loc='upper left', fontsize='small')
     
-    df_corr_energia = df[[escolha_coluna] + colunas[1]].groupby(escolha_coluna).agg(lambda x: sum(x))
+    df_corr_energia = df[[escolha_coluna] + colunas_energia].groupby(escolha_coluna).agg(lambda x: sum(x))
     df_corr_energia = df_corr_energia.rename(index = idx)
     df_corr_energia.plot(kind='bar', stacked=True, figsize=(12, 10), ax=ax[0,1])
     ax[0, 1].tick_params(axis='x', labelrotation=0)
     ax[0, 1].legend(loc='upper left', fontsize='small')
     
-    df_corr_esgoto = df[[escolha_coluna] + colunas[2]].groupby(escolha_coluna).agg(lambda x: sum(x))
+    df_corr_esgoto = df[[escolha_coluna] + colunas_esgoto].groupby(escolha_coluna).agg(lambda x: sum(x))
     df_corr_esgoto = df_corr_esgoto.rename(index = idx)
     df_corr_esgoto.plot(kind='bar', stacked=True, figsize=(12, 10), ax=ax[1,0])
     ax[1, 0].tick_params(axis='x', labelrotation=0)
     ax[1, 0].legend(loc='upper left', fontsize='small')
     
-    df_corr_lixo = df[[escolha_coluna] + colunas[3]].groupby(escolha_coluna).agg(lambda x: sum(x))
+    df_corr_lixo = df[[escolha_coluna] + colunas_lixo].groupby(escolha_coluna).agg(lambda x: sum(x))
     df_corr_lixo = df_corr_lixo.rename(index = idx)
     df_corr_lixo.plot(kind='bar', stacked=True, figsize=(12, 10), ax=ax[1,1])
     ax[1, 1].tick_params(axis='x', labelrotation=0)
@@ -124,5 +128,5 @@ def viz_correlativa(df: pd.DataFrame, escolha_coluna: str, colunas: list):
     plt.show()
 
 
-def viz_clusterizada(df: pd.DataFrame):
+def viz_clusterizada(df: pd.DataFrame, escolha_coluna: str, colunas: list):
     pass # a fazer...
