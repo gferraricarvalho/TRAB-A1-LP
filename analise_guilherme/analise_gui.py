24cb1640015f2c523d9189ff49cb782f13910500
gferraricarvalho <guiferraricarvalho.gf@gmail.com>
2023-10-14 17:47:58 -0300

    Comentarios adicionados

--- analise_guilherme/analise_gui.py
+++ analise_guilherme/analise_gui.py
@@ -4,22 +4,27 @@ import matplotlib.pyplot as plt
 import clean_columns as cc
 import graph_function as gf
 
-# possibilita ler arquivos/módulos um nível acima
+#Possibilita ler arquivos/módulos um nível acima
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))) 
 import ler_arquivo as la
 
-# encontra a base de dados
+#Encontra a base de dados
 caminho_arquivo = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', "microdados_ed_basica_2021.csv")
 
 df_original = la.ler_arquivo_csv(caminho_arquivo)
 
+#Define as colunas que serão mantidas
 colunas_filtradas = ['IN_ACESSIBILIDADE_CORRIMAO', 'IN_ACESSIBILIDADE_ELEVADOR',	'IN_ACESSIBILIDADE_VAO_LIVRE', 	'IN_ACESSIBILIDADE_RAMPAS',	'IN_ACESSIBILIDADE_SINAL_VISUAL',
                      'IN_ACESSIBILIDADE_SINAL_SONORO',	'IN_ACESSIBILIDADE_PISOS_TATEIS', 'IN_ACESSIBILIDADE_SINAL_TATIL', 'IN_ACESSIBILIDADE_INEXISTENTE']
 
+#Gera o dataframe com apenas as colunas selecionadas anteriormente
 df_filtrado = cc.filter_columns(df_original, colunas_filtradas)
 
+#Define novos rotulos para melhor organização e exibição das colunas
 novo_rotulo = ['Corrimão', 'Elevador', 'Vão Livre', 'Rampas', 'Sinal Visual', 'Sinal Sonoro', 'Pisos Táteis', 'Sinal Tátil', 'Inexistente']
 
+#Somas as colunas, buscando os valores totais para cada coluna
 df_acessibilidade = cc.sum_columns(df_filtrado)
 
+#Gera um gráfico em barra, com os valores e tipos de acessibilidade 
 grafico = gf.generate_graph_bar((10,6), df_acessibilidade, (0,100000), "Acessibilidade (221140 Escolas Analisadas)", "N° de Escolas", "Tipo de Acessibilidade", "black", novo_rotulo, "acessibilidade.png")
--- analise_guilherme/graph_function.py
+++ analise_guilherme/graph_function.py
@@ -30,19 +30,26 @@ def generate_graph_bar(figuresize, dataframe, x_lim, titulo, x_label, y_label, c
     Apenas exibe o gráfico, e salva em png.
 
     """
+    # Gera a figura do gráfico 
     fig, ax = plt.subplots(figsize= figuresize)
     dataframe.plot( kind= "barh", xlim= x_lim)
+
+    # Adiciona o título da figura, e os títulos dos eixos
     plt.title(titulo)
     plt.xlabel(x_label)
     plt.ylabel(y_label)
 
+    # Adiciona os valores individuais em cada barra do gráfico
     for i, v in enumerate(dataframe):
         ax.text(v, i, str(v), color= color_values, va='center')
 
-
+    # Coloca os novos nomes para as colunas
     ax.set_yticklabels(new_label)
 
+    # Desloca a figura à direita para melhor visualização
     plt.subplots_adjust(left=0.2)
+
+    #Plota e salva a figura no formato png
     plt.show()
     plt.savefig(namefig)
     return
