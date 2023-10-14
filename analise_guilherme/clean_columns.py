ac87d4964645bd39b3678c5509b58073a47c5318
gferraricarvalho <guiferraricarvalho.gf@gmail.com>
2023-10-14 17:00:43 -0300

    Docstring adicionado

--- analise_guilherme/clean_columns.py
+++ analise_guilherme/clean_columns.py
@@ -1,7 +1,38 @@
 def filter_columns(dataframe, colunas):
+    """
+    A função filtra as colunas de um dataframe, 
+    mantendo apenas as selecionadas pelo usuário.
+
+    Parameters
+    ----------
+    dataframe : dataframe
+        Dataframe original para filtrar as colunas.
+    colunas : list
+        Lista com as colunas que deseja manter no dataframe.
+
+    Returns
+    -------
+    df_filtrado : dataframe
+        Dataframe filtrado com as colunas selecionadas.
+
+    """
     df_filtrado = dataframe[colunas]
     return df_filtrado
 
 def sum_columns(dataframe):
+    """
+    Somas as colunas do dataframe.
+
+    Parameters
+    ----------
+    dataframe : dataframe
+        Dataframe escolhido para somar colunas.
+
+    Returns
+    -------
+    df_somado : dataframe
+        Dataframe que exibe a soma das colunas.
+
+    """
     df_somado = dataframe.sum()
     return df_somado
--- analise_guilherme/graph_function.py
+++ analise_guilherme/graph_function.py
@@ -1,7 +1,35 @@
 import matplotlib.pyplot as plt
 
 def generate_graph_bar(figuresize, dataframe, x_lim, titulo, x_label, y_label, color_values, new_label, namefig):
+    """
+    Função para gerar o gráfico de barras.
 
+    Parameters
+    ----------
+    figuresize : tuple
+        Tamanho da figura que será gerada.
+    dataframe : dataframe
+        Dataframe de dados original.
+    x_lim : tuple
+        Valores que definem os intervalos do eixo X.
+    titulo : string
+        Título da figura gerada.
+    x_label : string
+        Título do eixo X.
+    y_label : string
+        Título do eixo Y.
+    color_values : string
+        Define a cor dos valores individuais de cada coluna.
+    new_label : list
+        Novos nomes das colunas.
+    namefig : string
+        Nome da figura.
+
+    Returns
+    -------
+    Apenas exibe o gráfico, e salva em png.
+
+    """
     fig, ax = plt.subplots(figsize= figuresize)
     dataframe.plot( kind= "barh", xlim= x_lim)
     plt.title(titulo)
