ffc15f5e191da9c5567479f537f1f082cbff7c42
Edgard-junio <edgard.pic2019@gmail.com>
2023-10-14 22:24:14 -0300

    mudando os parâmetros da analise

--- analise_edgard/analise.py
+++ analise_edgard/analise.py
@@ -1,4 +1,6 @@
+import pandas as pd
 import visualizacao as vz
+from limpeza import funcoes as fl
 
 def analise_completa():
     """
@@ -13,29 +15,34 @@ def analise_completa():
 
     #localizando a base de dados
     colunas = ['SG_UF','NO_MUNICIPIO','IN_BANHEIRO_PNE', 'IN_BIBLIOTECA',
-        'IN_DORMITORIO_ALUNO', 'IN_DORMITORIO_PROFESSOR', 'IN_QUADRA_ESPORTES'
+        'IN_DORMITORIO_ALUNO','IN_REFEITORIO', 'IN_QUADRA_ESPORTES'
         ]
-    df = vz.localizacao('microdados_ed_basica_2021.csv')
-  
+    df = vz.localizacao('dados_limpos.csv')
+    #frequencia com que cada estado aparece
+    freq = vz.freq_contagem(df,'SG_UF').sort_index()
+
     df_modificado = vz.organiza('SG_UF',df,colunas)
 
     """Aqui chamamos a função vz.visualização_RG para plotar todos os gráficos que serão usados"""
-    vz.visualização_RG(df_modificado,'IN_BIBLIOTECA','escolas com bibliotecas',df_modificado['SG_UF'],'Escolas com bibliotecas por Unidade da Federação','imagem_1')
-    vz.visualização_RG(df_modificado,'IN_BANHEIRO_PNE','quantidade de banheiros adaptados',df_modificado['SG_UF'],'quantidade de escolas com banheiros adaptados','imagem_2')
-    vz.visualização_RG(df_modificado,'IN_DORMITORIO_ALUNO','dormitórios para alunos',df_modificado['SG_UF'],'Escolas dormitórios para alunos por Unidade da Federação','imagem_3')
-    vz.visualização_RG(df_modificado,'IN_DORMITORIO_PROFESSOR','dormitórios para professores',df_modificado['SG_UF'],'Escolas com dormitórios para professores por Unidade da Federação','imagem_4')
-    vz.visualização_RG(df_modificado,'IN_QUADRA_ESPORTES','quadras esportivas',df_modificado['SG_UF'],'Escolas com quadras esportivas por Unidade da Federação','imagem_5')
+    vz.visualização_RG(df_modificado,'IN_BIBLIOTECA','escolas com bibliotecas',df_modificado['SG_UF'],'Escolas com bibliotecas por Unidade da Federação','imagem_1',freq)
+    vz.visualização_RG(df_modificado,'IN_BANHEIRO_PNE','quantidade de banheiros adaptados',df_modificado['SG_UF'],'quantidade de escolas com banheiros adaptados','imagem_2',freq)
+    vz.visualização_RG(df_modificado,'IN_DORMITORIO_ALUNO','dormitórios para alunos',df_modificado['SG_UF'],'Escolas dormitórios para alunos por Unidade da Federação','imagem_3',freq)
+    vz.visualização_RG(df_modificado,'IN_REFEITORIO','refeitorias em escolas',df_modificado['SG_UF'],'Escolas com refeitórios','imagem_4',freq)
+    vz.visualização_RG(df_modificado,'IN_QUADRA_ESPORTES','quadras esportivas',df_modificado['SG_UF'],'Escolas com quadras esportivas por Unidade da Federação','imagem_5',freq)
+
 
     """separando o dataframe por unidade de federação, 
         apenas aquelas que tiveram um grande resultado em relação as demais em cada um dos graficos"""
-    df_sp = df.loc[df['SG_UF'] == 'SP'] #Destaque em banheiros adaptados e quadras esportivas
-    df_mg = df.loc[df['SG_UF'] == 'MG'] #Destaque em bibliotecas e dormitorios para alunos
-    df_pa = df.loc[df['SG_UF'] == 'PA'] #Destaque em dormitorios para professores
+    df_ac = df.loc[df['SG_UF'] == 'AC'] #Menor proporção entre quantidade de escolas totais e escolas com bibliotecas
+    df_am = df.loc[df['SG_UF'] == 'AM'] #Menor proporção entre quantidade de escolas totais e escolas com banheiros adaptados 
+    df_sp = df.loc[df['SG_UF'] == 'SP'] #Menor proporção entre quantidade de escolas totais e escolas com dormitórios para alunos
+    df_ma = df.loc[df['SG_UF'] == 'MA'] #Menor proporção entre quantidade de escolas totais e escolas com refeitórios e quadras esportivas
 
     #organizando cada um deles por municipio
+    df_ac = vz.organiza('NO_MUNICIPIO',df_ac,colunas)
+    df_am = vz.organiza('NO_MUNICIPIO',df_am,colunas)
     df_sp = vz.organiza('NO_MUNICIPIO',df_sp,colunas)
-    df_mg = vz.organiza('NO_MUNICIPIO',df_mg,colunas)
-    df_pa = vz.organiza('NO_MUNICIPIO',df_pa,colunas)
+    df_ma = vz.organiza('NO_MUNICIPIO',df_ma,colunas)
 
     # Criar um dicionário para armazenar as estatísticas de diferentes estados
     estatisticas = {}
@@ -47,33 +54,41 @@ def analise_completa():
         'max_quadra': vz.encontrar_max(df_sp, 'IN_QUADRA_ESPORTES','NO_MUNICIPIO'),
         'max_biblioteca': vz.encontrar_max(df_sp, 'IN_BIBLIOTECA','NO_MUNICIPIO'),
         'max_dorm_aluno': vz.encontrar_max(df_sp, 'IN_DORMITORIO_ALUNO','NO_MUNICIPIO'),
-        'max_dorm_professor': vz.encontrar_max(df_sp, 'IN_DORMITORIO_PROFESSOR','NO_MUNICIPIO')
+        'max_refeitório': vz.encontrar_max(df_sp, 'IN_REFEITORIO','NO_MUNICIPIO')
     }
 
     # Calcular as estatísticas para Minas Gerais e armazená-las no dicionário
-    estatisticas['MG'] = {
-        'estatisticas': vz.calcular_estatisticas(df_mg),
-        'max_banheiro': vz.encontrar_max(df_mg, 'IN_BANHEIRO_PNE','NO_MUNICIPIO'),
-        'max_quadra': vz.encontrar_max(df_mg, 'IN_QUADRA_ESPORTES','NO_MUNICIPIO'),
-        'max_biblioteca': vz.encontrar_max(df_mg, 'IN_BIBLIOTECA','NO_MUNICIPIO'),
-        'max_dorm_aluno': vz.encontrar_max(df_mg, 'IN_DORMITORIO_ALUNO','NO_MUNICIPIO'),
-        'max_dorm_professor': vz.encontrar_max(df_mg, 'IN_DORMITORIO_PROFESSOR','NO_MUNICIPIO')
+    estatisticas['MA'] = {
+        'estatisticas': vz.calcular_estatisticas(df_ma),
+        'max_banheiro': vz.encontrar_max(df_ma, 'IN_BANHEIRO_PNE','NO_MUNICIPIO'),
+        'max_quadra': vz.encontrar_max(df_ma, 'IN_QUADRA_ESPORTES','NO_MUNICIPIO'),
+        'max_biblioteca': vz.encontrar_max(df_ma, 'IN_BIBLIOTECA','NO_MUNICIPIO'),
+        'max_dorm_aluno': vz.encontrar_max(df_ma, 'IN_DORMITORIO_ALUNO','NO_MUNICIPIO'),
+        'max_refeitório': vz.encontrar_max(df_ma, 'IN_REFEITORIO','NO_MUNICIPIO')
+    }
+    estatisticas['AM'] = {
+        'estatisticas': vz.calcular_estatisticas(df_ma),
+        'max_banheiro': vz.encontrar_max(df_am, 'IN_BANHEIRO_PNE','NO_MUNICIPIO'),
+        'max_quadra': vz.encontrar_max(df_am, 'IN_QUADRA_ESPORTES','NO_MUNICIPIO'),
+        'max_biblioteca': vz.encontrar_max(df_am, 'IN_BIBLIOTECA','NO_MUNICIPIO'),
+        'max_dorm_aluno': vz.encontrar_max(df_am, 'IN_DORMITORIO_ALUNO','NO_MUNICIPIO'),
+        'max_refeitório': vz.encontrar_max(df_am, 'IN_REFEITORIO','NO_MUNICIPIO')
+    }
+    estatisticas['AC'] = {
+        'estatisticas': vz.calcular_estatisticas(df_ac),
+        'max_banheiro': vz.encontrar_max(df_ac, 'IN_BANHEIRO_PNE','NO_MUNICIPIO'),
+        'max_quadra': vz.encontrar_max(df_ac, 'IN_QUADRA_ESPORTES','NO_MUNICIPIO'),
+        'max_biblioteca': vz.encontrar_max(df_ac, 'IN_BIBLIOTECA','NO_MUNICIPIO'),
+        'max_dorm_aluno': vz.encontrar_max(df_ac, 'IN_DORMITORIO_ALUNO','NO_MUNICIPIO'),
+        'max_refeitório': vz.encontrar_max(df_ac, 'IN_REFEITORIO','NO_MUNICIPIO')
     }
 
-    # Calcular as estatísticas para o Pará e armazená-las no dicionário
-    estatisticas['PA'] = {
-        'estatisticas': vz.calcular_estatisticas(df_pa),
-        'max_banheiro': vz.encontrar_max(df_pa, 'IN_BANHEIRO_PNE','NO_MUNICIPIO'),
-        'max_quadra': vz.encontrar_max(df_pa, 'IN_QUADRA_ESPORTES','NO_MUNICIPIO'),
-        'max_biblioteca': vz.encontrar_max(df_pa, 'IN_BIBLIOTECA','NO_MUNICIPIO'),
-        'max_dorm_aluno': vz.encontrar_max(df_pa, 'IN_DORMITORIO_ALUNO','NO_MUNICIPIO'),
-        'max_dorm_professor': vz.encontrar_max(df_pa, 'IN_DORMITORIO_PROFESSOR','NO_MUNICIPIO')
-    }
-    
-    return estatisticas
+    print(estatisticas)
 
 
 
+if __name__ == "__main__":
+    analise_completa()
 
 
 
--- analise_edgard/main.py
+++ analise_edgard/main.py
@@ -2,7 +2,9 @@ import analise as an
 import pandas as pd
 
 def main():
-    analise = pd.DataFrame(an.analise_completa())
+    """Roda a função analise_completa do arquivo analise.py, e depois converte o dicionario para um DataFrame"""
+    analise =  an.analise_completa()
+    print(analise)
 
     return analise
     
