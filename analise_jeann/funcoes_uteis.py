6bc79f3e66ca5d7ef9ef0867c7797255e8c5bf3e
Jeann-Rocha <jeannrochasilva@gmail.com>
2023-10-15 04:05:36 -0300

    Atualizando os módulos de funções úteis e vizualização

--- analise_jeann/funcoes_uteis.py
+++ analise_jeann/funcoes_uteis.py
@@ -1,73 +1,136 @@
+import os
 import pandas as pd
+import matplotlib.pyplot as plt
 
 def decora_print(mensagem: str):
     """ Decora os prints de tela no decorrer do código
+
+    Parameters:
+    -----------
+    mensagem: Texto que será exibido na tela.
     """
     print()
-    print("#" * 100)
+    print("#" * 120)
     print(mensagem)
-    print("#" * 100)
+    print("#" * 120)
     print()
 
 
 def escolhe_colunas(df: pd.DataFrame, colunas: list) -> pd.DataFrame:
     """Retorna um dataframe apenas com as colunas que serão utilizadas
+
+    Parameters:
+    -----------
+    mensagem: Texto que será exibido na tela.
     """
     return df[colunas]
 
 
-def agrupa_por(df: pd.DataFrame, escolha_coluna: str, colunas: list) -> pd.DataFrame:
+def agrupa_por_sum(df: pd.DataFrame, escolha_coluna: str, colunas: list) -> pd.DataFrame:
     """Retorna um dataframe com os indíces agrupados por uma coluna base e os valores numéricos das colunas informativas somados
     nesse agrupamento.
+
+    Parameters:
+    -----------
+    df: dataframe.
+    escolha_colunas: coluna base.
+    colunas: lista de colunas padrão (indicadores ecológicos)
     """
 
-    if escolha_coluna not in ['NO_REGIAO', 'SG_UF', 'NO_MUNICIPIO', 'TP_LOCALIZACAO', 'TP_LOCALIZACAO_DIFERENCIADA', 'TP_DEPENDENCIA']:
-        raise ValueError('A escolha_coluna deve ser "NO_REGIAO" (Nome da Região),  "SG_UF" (Sigla do Estado), "NO_MUNICIPIO" (Nome do Município), "TP_LOCALIZACAO" (Tipo de Localização da Escola), "TP_LOCALIZACAO_DIFERENCIADA" (Tipo de Localização Diferenciada da Escola) ou "TP_DEPENDÊNCIA" (Tipo de Dependência da Escola)')
+    if escolha_coluna not in ['NO_REGIAO', 'SG_UF', 'TP_LOCALIZACAO', 'TP_LOCALIZACAO_DIFERENCIADA', 'TP_DEPENDENCIA']:
+        raise ValueError('A escolha_coluna deve ser "NO_REGIAO" (Nome da Região),  "SG_UF" (Sigla do Estado), "TP_LOCALIZACAO" (Tipo de Localização da Escola), "TP_LOCALIZACAO_DIFERENCIADA" (Tipo de Localização Diferenciada da Escola) ou "TP_DEPENDÊNCIA" (Tipo de Dependência da Escola)')
     
     return df[[escolha_coluna] + colunas].groupby(escolha_coluna).agg(lambda x: sum(x))
 
 
-def dados_descritivos(df: pd.DataFrame, escolha_coluna: str, colunas: list) -> pd.DataFrame:
-    """ Dados descritivos gerais para o conjunto de dados em questão.
+def agrupa_por_cont(df: pd.DataFrame, escolha_coluna: str, colunas: list) -> pd.DataFrame:
+    """Retorna um dataframe com os indíces agrupados por uma coluna base e os valores numéricos das colunas informativas contados
+    nesse agrupamento.
+
+    Parameters:
+    -----------
+    df: dataframe.
+    escolha_colunas: coluna base.
+    colunas: lista de colunas padrão (indicadores ecológicos)
     """
 
-    if escolha_coluna not in ['NO_REGIAO', 'SG_UF', 'NO_MUNICIPIO']:
-        raise ValueError('A escolha_coluna deve ser "NO_REGIAO" (Nome da Região),  "SG_UF" (Sigla do Estado) ou "NO_MUNICIPIO" (Nome do Município)')
+    if escolha_coluna not in ['NO_REGIAO', 'SG_UF', 'TP_LOCALIZACAO', 'TP_LOCALIZACAO_DIFERENCIADA', 'TP_DEPENDENCIA']:
+        raise ValueError('A escolha_coluna deve ser "NO_REGIAO" (Nome da Região),  "SG_UF" (Sigla do Estado), "TP_LOCALIZACAO" (Tipo de Localização da Escola), "TP_LOCALIZACAO_DIFERENCIADA" (Tipo de Localização Diferenciada da Escola) ou "TP_DEPENDÊNCIA" (Tipo de Dependência da Escola)')
     
-    min = agrupa_por(df, escolha_coluna, colunas).min() # mínimo dos valores
-    max = agrupa_por(df, escolha_coluna, colunas).max() # máximo dos valores
-    mean = agrupa_por(df, escolha_coluna, colunas).mean() # media dos valores
-    median = agrupa_por(df, escolha_coluna, colunas).median() # mediana dos valores
-    std = agrupa_por(df, escolha_coluna, colunas).std() # desvio padrão dos valores
+    return df[[escolha_coluna] + colunas].groupby(escolha_coluna).agg(lambda x: len(x))
+
+
+def agrupa_por_proporcao(df: pd.DataFrame, escolha_coluna: str, colunas: list) -> pd.DataFrame:
+    """Retorna um dataframe com os indíces agrupados por uma coluna base e os valores numéricos das colunas informativas em proporção
+    nesse agrupamento.
+
+    Parameters:
+    -----------
+    df: dataframe.
+    escolha_colunas: coluna base.
+    colunas: lista de colunas padrão (indicadores ecológicos)
+    """
+
+    if escolha_coluna not in ['NO_REGIAO', 'SG_UF', 'TP_LOCALIZACAO', 'TP_LOCALIZACAO_DIFERENCIADA', 'TP_DEPENDENCIA']:
+        raise ValueError('A escolha_coluna deve ser "NO_REGIAO" (Nome da Região),  "SG_UF" (Sigla do Estado), "TP_LOCALIZACAO" (Tipo de Localização da Escola), "TP_LOCALIZACAO_DIFERENCIADA" (Tipo de Localização Diferenciada da Escola) ou "TP_DEPENDÊNCIA" (Tipo de Dependência da Escola)')
+    
+    return agrupa_por_sum(df, escolha_coluna, colunas) / agrupa_por_cont(df, escolha_coluna, colunas)
+
+
+def salva_dataset(df: pd.DataFrame, name: str):
+    # fazer docstring e doctest
+    if not os.path.exists('analise_jeann/datasets'):
+        os.makedirs('analise_jeann/datasets')
+    df.to_csv(f"analise_jeann/datasets/{name}.csv", sep=';')
+
+
+def salva_imagens(fig, name: str):
+    # fazer docstring e doctest
+    if not os.path.exists('analise_jeann/imagens'):
+        os.makedirs('analise_jeann/imagens')
+    fig.savefig(f"analise_jeann/imagens/{name}.png")
+
+
+def dados_descritivos(df: pd.DataFrame, escolha_coluna: str, colunas: list) -> pd.DataFrame:
+    """ Dados descritivos gerais para o conjunto de dados em questão.
+
+    Parameters:
+    -----------
+    df: dataframe.
+    escolha_colunas: coluna base.
+    colunas: lista de colunas padrão (indicadores ecológicos)
+    """
+
+    if escolha_coluna not in ['NO_REGIAO', 'SG_UF']:
+        raise ValueError('A escolha_coluna deve ser "NO_REGIAO" (Nome da Região) ou "SG_UF" (Sigla do Estado)')
+    
+    min = agrupa_por_sum(df, escolha_coluna, colunas).min() # mínimo dos valores
+    max = agrupa_por_sum(df, escolha_coluna, colunas).max() # máximo dos valores
+    mean = agrupa_por_sum(df, escolha_coluna, colunas).mean() # media dos valores
+    median = agrupa_por_sum(df, escolha_coluna, colunas).median() # mediana dos valores
+    std = agrupa_por_sum(df, escolha_coluna, colunas).std() # desvio padrão dos valores
     
     dados = pd.concat([min, max, mean, median, std], axis=1).T # cria um dataframe dos dados estatísticos feitos acima
     dados.index = ['MINIMO', 'MAXIMO', 'MEDIA', 'MEDIANA', 'DESVIO PADRAO']
     return dados
 
 
-def acima_media(df: pd. DataFrame, escolha_coluna: str, colunas: list) -> pd.DataFrame:
+def acima_media(df: pd. DataFrame, escolha_coluna: str, colunas: list):
     """Retorna os locais (em Regiões ou em Estados) que estão acima do valor médio em cada indicador.
+
+    Parameters:
+    -----------
+    df: dataframe.
+    escolha_colunas: coluna base.
+    colunas: lista de colunas padrão (indicadores ecológicos)
     """
 
-    if escolha_coluna not in ['NO_REGIAO', 'SG_UF', 'NO_MUNICIPIO']:
-        raise ValueError('A escolha_coluna deve ser "NO_REGIAO" (Nome da Região),  "SG_UF" (Sigla do Estado) ou "NO_MUNICIPIO" (Nome do Município)')
+    if escolha_coluna not in ['NO_REGIAO', 'SG_UF']:
+        raise ValueError('A escolha_coluna deve ser "NO_REGIAO" (Nome da Região) ou "SG_UF" (Sigla do Estado)')
     
-    dados = agrupa_por(df, escolha_coluna, colunas)
+    dados = agrupa_por_sum(df, escolha_coluna, colunas)
     d = dados_descritivos(df, escolha_coluna, colunas) 
     
     for col in colunas:
         media = d.loc[d.index == "MEDIA"][col].iloc[0]
         print(f"{col}: {dados.loc[dados[col] > media].sort_values(col).index.to_list()}")
-
-
-def dist_freq(df: pd.DataFrame, colunas: list):
-    """Retorna a Distribuição de Frequências de todas as colunas dadas de um dataframe fornecido
-    """
-    
-    for col in colunas:
-        print(df[col].value_counts().sort_index())
-        print()
-
-
-def calcula_proximos(df: pd.DataFrame, escolha_coluna: str) -> list:
-    pass # a fazer...
--- analise_jeann/vizualizacao.py
+++ analise_jeann/vizualizacao.py
@@ -1,89 +1,62 @@
+import os
 import funcoes_uteis as fu
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def viz_simples(df: pd.DataFrame, escolha_coluna: str, colunas: list, dado_especifico=""):
+def viz_simples(df: pd.DataFrame, escolha_coluna: str, colunas: list, quant_graf_lin: int, quant_graf_col: int, name: str):
     """Vizualização Simplificada (por Região ou Estado) da quantidade de cada indicador ecológico utilizado (Água, Energia, Esgoto ou Tratamento
     de Lixo) em um (ou vários) gráfico(s) de barras verticais.
     """
-
+    
     if escolha_coluna not in ['NO_REGIAO', 'SG_UF']:
         raise ValueError('A escolha_coluna deve ser "NO_REGIAO" (Nome da Região) ou "SG_UF" (Sigla do Estado)')
-
-    if dado_especifico not in [""] + colunas:
-        raise ValueError(f'O dado_específico deve ser vazio (nenhum valor atribuído ou string vazia) ou deve ser alguma das colunas: {colunas}')
     
-    df_tend = fu.agrupa_por(df, escolha_coluna, colunas)
+    if quant_graf_lin * quant_graf_col != len(colunas):
+        pass # escrever um raise
 
-    if dado_especifico == "":
-        fig, ax = plt.subplots(4, 6, figsize=(20, 10))
-        fig.subplots_adjust(hspace=0.5)
-        
+    df = fu.agrupa_por_sum(df, escolha_coluna, colunas)
+
+    fig, ax = plt.subplots(quant_graf_lin, quant_graf_col, figsize=(20, 10))
+    fig.subplots_adjust(hspace=0.5, wspace=0.5)
+    
+    if quant_graf_lin == quant_graf_col == 1:
+        media = df[colunas[0]].mean()
+        ax.bar(df.index, df[colunas[0]])
+        ax.set_title(f"{colunas[0]}", fontsize=15)
+        if escolha_coluna == 'SG_UF':
+            ax.tick_params(axis='x', labelrotation=90) # detalhes de formatação
+        ax.tick_params(axis='x', labelsize=10)
+        ax.tick_params(axis='y', labelsize=15)
+        ax.axhline(media, color='red', linestyle='-', label='Média')
+
+    elif quant_graf_lin == 1:
+        for j in range(0, quant_graf_col):
+            media = df[colunas[j]].mean()
+            ax[j].bar(df.index, df[colunas[j]])
+            ax[j].set_title(f"{colunas[j]}", fontsize=15)
+            if escolha_coluna == 'SG_UF':
+                ax[j].tick_params(axis='x', labelrotation=90) # detalhes de formatação
+            ax[j].tick_params(axis='x', labelsize=10)
+            ax[j].tick_params(axis='y', labelsize=12)
+            ax[j].axhline(media, color='red', linestyle='-', label='Média')
+    else:
         cont = 0
-        for i in range(0, 4):
-            for j in range(0, 6):
-                media = df_tend[colunas[cont]].mean()
-                ax[i, j].bar(df_tend.index, df_tend[colunas[cont]])
-                ax[i, j].set_title(f"{colunas[cont]}", fontsize=7)
+        for i in range(0, quant_graf_lin):
+            for j in range(0, quant_graf_col):
+                media = df[colunas[cont]].mean()
+                ax[i, j].bar(df.index, df[colunas[cont]])
+                ax[i, j].set_title(f"{colunas[cont]}", fontsize=15)
                 if escolha_coluna == 'SG_UF':
-                    ax[i, j].tick_params(axis='x', labelrotation=90)
-                ax[i, j].tick_params(axis='x', labelsize=4)
-                ax[i, j].tick_params(axis='y', labelsize=5)
+                    ax[i, j].tick_params(axis='x', labelrotation=90) # detalhes de formatação
+                ax[i, j].tick_params(axis='x', labelsize=10)
+                ax[i, j].tick_params(axis='y', labelsize=12)
                 ax[i, j].axhline(media, color='red', linestyle='-', label='Média')
                 cont += 1
-    
-        plt.show()
 
-    else:
-        media = df_tend[dado_especifico].mean()
-        plt.bar(df_tend.index, df_tend[dado_especifico])
-        plt.title(f"{dado_especifico}")
-        if escolha_coluna == 'SG_UF':
-            plt.xticks(rotation=90)
-        plt.axhline(media, color='red', linestyle='-', label='Média')
-        plt.show()
+    fu.salva_imagens(fig, name)
 
 
-def viz_dist_freq(df: pd.DataFrame, escolha_coluna: str, colunas: list, dado_especifico=""):
-    """Vizualização da Distribuição de Frequências (por Região, Estado ou Município) da quantidade de cada indicador ecológico
-    utilizado (Água, Energia, Esgoto ou Tratamento de Lixo) em um (ou vários) gráfico(s) de barras verticais.
-    """
-
-    if escolha_coluna not in ['NO_REGIAO', 'SG_UF', 'NO_MUNICIPIO']:
-        raise ValueError('A escolha_coluna deve ser "NO_REGIAO" (Nome da Região), "SG_UF" (Sigla do Estado) ou "NO_MUNICIPIO" (Nome do Município)')
-
-    if dado_especifico not in [""] + colunas:
-        raise ValueError(f'O dado_específico deve ser vazio (nenhum valor atribuído ou string vazia) ou deve ser alguma das colunas: {colunas}')
-        
-    df_freq = fu.agrupa_por(df, escolha_coluna, colunas)
-    
-    if dado_especifico == "":
-        fig, ax = plt.subplots(4, 6, figsize=(40, 20))
-        fig.subplots_adjust(hspace=0.5, wspace=0.5)
-        
-        cont = 0
-        for i in range(0, 4):
-            for j in range(0, 6):
-                ax[i, j].hist(df_freq[colunas[cont]], bins=10, edgecolor='k')
-                ax[i, j].set_title(f"{colunas[cont]}", fontsize=7)
-                ax[i, j].set_xlabel("Quantidade", fontsize=7)
-                ax[i, j].set_ylabel("Frequência", fontsize=7)
-                ax[i, j].tick_params(axis='x', labelsize=4)
-                ax[i, j].tick_params(axis='y', labelsize=4)
-                cont += 1
-    
-        plt.show()
-
-    else:
-        df_freq = df_freq[dado_especifico].value_counts().sort_index()
-        plt.hist(df_freq, bins=10, edgecolor='k')
-        plt.xlabel("Quantidade")
-        plt.ylabel("Frequência")
-        plt.title(f"{dado_especifico}")
-        plt.show()
-
-
-def viz_correlativa(df: pd.DataFrame, escolha_coluna: str, colunas_agua: list, colunas_energia: list, colunas_esgoto: list, colunas_lixo: list):
+def viz_correlativa(df: pd.DataFrame, escolha_coluna: str, colunas_agua: list, colunas_energia: list, colunas_esgoto: list, colunas_lixo: list, name: str):
     """Vizualização da Correlação do Tipo de Localização, Tipo de Localização Diferenciada ou Tipo de Dependência com a
     quantidade de cada indicador ecológico utilizado (Água, Energia, Esgoto ou Tratamento de Lixo) em um (ou vários) gráfico(s)
     de barras verticais empilhadas.
@@ -100,33 +73,26 @@ def viz_correlativa(df: pd.DataFrame, escolha_coluna: str, colunas_agua: list, c
         idx = {1: "Federal", 2: "Estadual", 3: "Municipal", 4: "Privada"}
     
     fig, ax = plt.subplots(2, 2, figsize=(10, 8))
+    fig.subplots_adjust(hspace=0.5)
     
-    df_corr_agua = df[[escolha_coluna] + colunas_agua].groupby(escolha_coluna).agg(lambda x: sum(x))
-    df_corr_agua = df_corr_agua.rename(index = idx)
+    df_corr_agua = fu.agrupa_por_proporcao(df, escolha_coluna, colunas_agua) # proporcao da quantidade de escolas no quesito agua
     df_corr_agua.plot(kind='bar', stacked=True, figsize=(12, 10), ax=ax[0,0])
     ax[0, 0].tick_params(axis='x', labelrotation=0)
     ax[0, 0].legend(loc='upper left', fontsize='small')
     
-    df_corr_energia = df[[escolha_coluna] + colunas_energia].groupby(escolha_coluna).agg(lambda x: sum(x))
-    df_corr_energia = df_corr_energia.rename(index = idx)
+    df_corr_energia = fu.agrupa_por_proporcao(df, escolha_coluna, colunas_energia) # proporcao da quantidade de escolas no quesito energia
     df_corr_energia.plot(kind='bar', stacked=True, figsize=(12, 10), ax=ax[0,1])
     ax[0, 1].tick_params(axis='x', labelrotation=0)
     ax[0, 1].legend(loc='upper left', fontsize='small')
     
-    df_corr_esgoto = df[[escolha_coluna] + colunas_esgoto].groupby(escolha_coluna).agg(lambda x: sum(x))
-    df_corr_esgoto = df_corr_esgoto.rename(index = idx)
+    df_corr_esgoto = fu.agrupa_por_proporcao(df, escolha_coluna, colunas_esgoto) # proporcao da quantidade de escolas no quesito esgoto
     df_corr_esgoto.plot(kind='bar', stacked=True, figsize=(12, 10), ax=ax[1,0])
     ax[1, 0].tick_params(axis='x', labelrotation=0)
     ax[1, 0].legend(loc='upper left', fontsize='small')
     
-    df_corr_lixo = df[[escolha_coluna] + colunas_lixo].groupby(escolha_coluna).agg(lambda x: sum(x))
-    df_corr_lixo = df_corr_lixo.rename(index = idx)
+    df_corr_lixo = fu.agrupa_por_proporcao(df, escolha_coluna, colunas_lixo) # proporcao da quantidade de escolas no quesito lixo
     df_corr_lixo.plot(kind='bar', stacked=True, figsize=(12, 10), ax=ax[1,1])
     ax[1, 1].tick_params(axis='x', labelrotation=0)
     ax[1, 1].legend(loc='upper left', fontsize='small')
 
-    plt.show()
-
-
-def viz_clusterizada(df: pd.DataFrame, escolha_coluna: str, colunas: list):
-    pass # a fazer...
+    fu.salva_imagens(fig, name)
