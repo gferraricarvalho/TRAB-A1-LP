ac37a3add946636c88a44ed710170b6637538021
AlessandraBello <alessandrabellosoares@gmail.com>
2023-10-15 17:39:43 -0300

    grafico atualizado

--- analise_alessandra/analise_ale.py
+++ analise_alessandra/analise_ale.py
@@ -13,12 +13,10 @@ caminho_arquivo = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..',
 df_0 = la.ler_arquivo_csv(caminho_arquivo)
 
 #selecionando as colunas utilizadas para a análise
-df = fs.selecionar_colunas(df_0, "TP_DEPENDENCIA", "QT_MAT_BAS_ND", "QT_MAT_BAS_BRANCA", "QT_MAT_BAS_PRETA", "QT_MAT_BAS_PARDA", "QT_MAT_BAS_AMARELA", "QT_MAT_BAS_INDIGENA")
+df = fs.selecionar_colunas(df_0, "TP_DEPENDENCIA", "QT_MAT_BAS_ND", "QT_MAT_BAS_BRANCA", "QT_MAT_BAS_PRETA", 
+                           "QT_MAT_BAS_PARDA", "QT_MAT_BAS_AMARELA", "QT_MAT_BAS_INDIGENA")
 
-#alterando os valores, segundo dados fornecidos pela base de dados
-df = fs.trocar_valor(df, "TP_DEPENDENCIA", {1:"Federal", 2 : "Municipal", 3: "Estadual", 4:"Privada"})
-
-#contando o número de matriculas por raça
+#contando o número de matriculas por raça, foi dividido por um milhão para facilitar a visualização
 nao_declarada = (fs.somar_valores_agrupado(df, "QT_MAT_BAS_ND", "TP_DEPENDENCIA"))/1000000
 branca = (fs.somar_valores_agrupado(df, "QT_MAT_BAS_BRANCA", "TP_DEPENDENCIA"))/1000000
 preta = (fs.somar_valores_agrupado(df, "QT_MAT_BAS_PRETA", "TP_DEPENDENCIA"))/1000000
@@ -27,10 +25,11 @@ amarela = (fs.somar_valores_agrupado(df, "QT_MAT_BAS_AMARELA", "TP_DEPENDENCIA")
 indigena = (fs.somar_valores_agrupado(df, "QT_MAT_BAS_INDIGENA", "TP_DEPENDENCIA"))/1000000
 
 #plotando o gráfico
-grafico = fg.grafico_de_barras_6_var(0.13, nao_declarada, branca, preta, parda, amarela, indigena, "Não declarada", "Branca", 
-                                    "Preta", "Parda", "Amarela", "Indígena", "#A569BD", "#F4D03F", "#3498DB", "#E74C3C", 
-                                    "#58D68D", "#7F8C8D", 0, 10, "Número de matrículas na educação básica, por tipo de escola",
-                                    "Tipo de escola", ["Estadual", "Federal", "Municipal", "Privada"] ,"Número de matriculas (em milhões)", "analise_alessandra/grafico_Ale.png")
+grafico = fg.grafico_de_barras_6_var(0.13, nao_declarada, branca, preta, parda, amarela, indigena, "Não declarada", 
+                                     "Branca", "Preta", "Parda", "Amarela", "Indígena", "#A569BD", "#F4D03F", "#3498DB", 
+                                     "#E74C3C", "#58D68D", "#7F8C8D", 0, 10, "Número de matrículas na educação básica, por tipo de escola",
+                                     "Tipo de escola", ["Federal", "Municipal","Estadual", "Privada"] ,"Número de matriculas (em milhões)", 
+                                     "analise_alessandra/grafico_Ale.png")
 
 #print dos dados, para análise
 print ("Matrículas de pessoas da raça Não declarada: ", nao_declarada, "Matrículas de pessoas da raça Branca: ", branca,
