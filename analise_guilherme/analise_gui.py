6a2f21eec4482597c16b60fe7fdfd9f7bf107a33
gferraricarvalho <guiferraricarvalho.gf@gmail.com>
2023-10-15 22:00:12 -0300

    Alteracoes grafico

--- analise_guilherme/analise_gui.py
+++ analise_guilherme/analise_gui.py
@@ -14,7 +14,7 @@ df_original = la.ler_arquivo_csv(caminho_arquivo)
 
 #Define as colunas que serão mantidas
 colunas_filtradas = ['IN_ACESSIBILIDADE_CORRIMAO', 'IN_ACESSIBILIDADE_ELEVADOR',	'IN_ACESSIBILIDADE_VAO_LIVRE', 	'IN_ACESSIBILIDADE_RAMPAS',	'IN_ACESSIBILIDADE_SINAL_VISUAL',
-                     'IN_ACESSIBILIDADE_SINAL_SONORO',	'IN_ACESSIBILIDADE_PISOS_TATEIS', 'IN_ACESSIBILIDADE_SINAL_TATIL', 'IN_ACESSIBILIDADE_INEXISTENTE', 'folme']
+                     'IN_ACESSIBILIDADE_SINAL_SONORO',	'IN_ACESSIBILIDADE_PISOS_TATEIS', 'IN_ACESSIBILIDADE_SINAL_TATIL', 'IN_ACESSIBILIDADE_INEXISTENTE']
 
 #Gera o dataframe com apenas as colunas selecionadas anteriormente
 df_filtrado = cc.filter_columns(df_original, colunas_filtradas)
--- analise_guilherme/graph_function.py
+++ analise_guilherme/graph_function.py
@@ -30,9 +30,19 @@ def generate_graph_bar(figuresize, dataframe, x_lim, titulo, x_label, y_label, c
     Apenas exibe o gráfico, e salva em png.
 
     """
+    # Mapeamento de cores para categorias
+    color_map = {'IN_ACESSIBILIDADE_CORRIMAO': "green", 'IN_ACESSIBILIDADE_ELEVADOR': "green",	
+                 'IN_ACESSIBILIDADE_VAO_LIVRE': "green", 'IN_ACESSIBILIDADE_RAMPAS': "green",	
+                 'IN_ACESSIBILIDADE_SINAL_VISUAL': "yellow", 'IN_ACESSIBILIDADE_SINAL_SONORO': "blue",
+                 'IN_ACESSIBILIDADE_PISOS_TATEIS': "blue", 'IN_ACESSIBILIDADE_SINAL_TATIL': "blue", 
+                 'IN_ACESSIBILIDADE_INEXISTENTE':"red"}
+
+    # Define uma lista de cores com base nas categorias
+    colors = [color_map[column] for column in dataframe.index]
+
     # Gera a figura do gráfico 
     fig, ax = plt.subplots(figsize= figuresize)
-    dataframe.plot( kind= "barh", xlim= x_lim)
+    dataframe.plot(kind="barh", xlim=x_lim, color=colors, legend=False, ax=ax)
 
     # Adiciona o título da figura, e os títulos dos eixos
     plt.title(titulo)
@@ -42,6 +52,12 @@ def generate_graph_bar(figuresize, dataframe, x_lim, titulo, x_label, y_label, c
     # Adiciona os valores individuais em cada barra do gráfico
     for i, v in enumerate(dataframe):
         ax.text(v, i, str(v), color= color_values, va='center')
+
+    rotulo_legenda = {"Deficiência Física": "green", "Deficiência Visual":"blue", "Deficiência Auditiva":"yellow","Sem Adaptação ou\n Acessibilidade":"red"}
+
+    # Cria uma legenda personalizada
+    legend_handles = [plt.Rectangle((0, 0), 1, 1, color=rotulo_legenda[cor]) for cor in rotulo_legenda]
+    plt.legend(legend_handles, rotulo_legenda, title='Legendas', loc='upper right')
 
     # Coloca os novos nomes para as colunas
     ax.set_yticklabels(new_label)
