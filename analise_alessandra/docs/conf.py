079b9798287b1ca070c49c7934138943bc340c4e
AlessandraBello <alessandrabellosoares@gmail.com>
2023-10-16 14:10:55 -0300

    teste sphinx

--- analise_alessandra/docs/conf.py
+++ analise_alessandra/docs/conf.py
@@ -3,9 +3,11 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import sphinx_rtd_theme
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../'))
+
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
@@ -29,4 +31,5 @@ language = 'pt_BR'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_static_path = ['_static']
